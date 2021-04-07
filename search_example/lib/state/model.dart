aa037f14029f073d4e63cf415c24deaa6cd75b58
VHCBlade <vhcblade@gmail.com>
2021-04-07 22:22:16 +0800

    Initial Commit

---
+++ search_example/lib/state/model.dart
@@ -0,0 +1,13 @@
+import 'package:search_example/state/event_channel.dart';
+
+abstract class Model {
+  /// Add functions to this to be ran when the model is updated.
+  List<void Function()> modelUpdated = [];
+
+  /// Events that the UI fire to affect this model are received through here.
+  ProviderEventChannel get eventChannel;
+
+  void updateModel() => modelUpdated.forEach((element) => element());
+
+  void dispose() {}
+}
